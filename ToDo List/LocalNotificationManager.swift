911ac32eed9236a48f1db49befe022d87fa1a0c2
Chris Burdick <burdicch@bc.edu>
2020-07-06 12:58:51 -0400

    Chapter 5.15 - reusable code with helper struct & static methods

---
+++ ToDo List/LocalNotificationManager.swift
@@ -0,0 +1,58 @@
+//
+//  LocalNotificationManager.swift
+//  ToDo List
+//
+//  Created by Chris Burdick on 7/6/20.
+//  Copyright © 2020 Chris Burdick. All rights reserved.
+//
+
+import Foundation
+import UserNotifications
+
+struct LocalNotificationManager {
+    
+    static func authorizeLocalNotifications() {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error)
+            in
+            guard error == nil else {
+                print("ERROR: \(error!.localizedDescription)")
+                return
+            }
+            if granted {
+                print("✅ Notifications Authorization Granted")
+            } else {
+                print("❌ Notifications Authorization Denied")
+                // TODO: put in alert telling user what to do
+            }
+        }
+    }
+    
+    static func setCalendarNotification(title: String, subtitle: String, body: String, badgeNumber: NSNumber?, sound: UNNotificationSound?, date: Date) -> String{
+        // create content
+        let content = UNMutableNotificationContent()
+        content.title = title
+        content.subtitle = subtitle
+        content.body = body
+        content.sound = sound
+        content.badge = badgeNumber
+        
+        // create trigger
+        var dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute], from: date)
+        dateComponents.second = 00
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+        
+        // create request
+        let notificationID = UUID().uuidString
+        let request = UNNotificationRequest(identifier: notificationID, content: content, trigger: trigger)
+        
+        // register request with notification center
+        UNUserNotificationCenter.current().add(request) { (error) in
+            if let error = error {
+                print("ERROR: \(error.localizedDescription)")
+            } else {
+                print("Notification scheduled \(notificationID), title: \(content.title)")
+            }
+        }
+        return notificationID
+    }
+}
--- ToDo List/ToDoItems.swift
+++ ToDo List/ToDoItems.swift
@@ -7,22 +7,11 @@
 //
 
 import Foundation
+import UserNotifications
 
 class ToDoItems {
     var itemsArray: [ToDoItem] = []
     
-    func saveData() {
-        let directoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
-        let documentURL = directoryURL.appendingPathComponent("todos").appendingPathExtension("json")
-        
-        let jsonEncoder = JSONEncoder()
-        let data = try? jsonEncoder.encode(itemsArray)
-        do {
-            try data?.write(to: documentURL, options: .noFileProtection)
-        } catch {
-            print("ERROR: could not save data \(error.localizedDescription)")
-        }
-    }
     func loadData(completed: @escaping () -> () ) {
         let directoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
         let documentURL = directoryURL.appendingPathComponent("todos").appendingPathExtension("json")
@@ -35,5 +24,35 @@ class ToDoItems {
             print("ERROR: could not load data \(error.localizedDescription)")
         }
         completed()
+    }
+    
+    func saveData() {
+        let directoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let documentURL = directoryURL.appendingPathComponent("todos").appendingPathExtension("json")
+        
+        let jsonEncoder = JSONEncoder()
+        let data = try? jsonEncoder.encode(itemsArray)
+        do {
+            try data?.write(to: documentURL, options: .noFileProtection)
+        } catch {
+            print("ERROR: could not save data \(error.localizedDescription)")
+        }
+        setNotifications() 
+    }
+    
+    func setNotifications() {
+        guard itemsArray.count > 0 else {
+            return
+        }
+        // remove all notifications
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+        
+        // re-create them with updated data that was just saved
+        for index in 0..<itemsArray.count {
+            if itemsArray[index].reminderSet {
+                let toDoItem = itemsArray[index]
+                itemsArray[index].notificationID = LocalNotificationManager.setCalendarNotification(title: toDoItem.name, subtitle: "", body: toDoItem.notes, badgeNumber: nil, sound: .default, date: toDoItem.date)
+            }
+        }
     }
 }
